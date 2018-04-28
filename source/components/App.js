eace53c7e7aa7ac69094c041bb515a4d4e5337ff
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-04-28 12:21:31 +0200

    Added Webpack and babel

---
+++ source/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return "React 16 & Webpack 3 rockzzz!";
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: ["./source/App.js"],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  }
+};
