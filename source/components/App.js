b34388da0ef9d1f85990e4d35b189d6b2ffae391
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-04-28 12:24:41 +0200

    Setup from component initializer

--- source/components/App.js
+++ source/components/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
+import Unity from "../../../React-Unity-WebGL/";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.myUnity = new Unity();
   }
   render() {
-    return "React 16 & Webpack 3 rockzzz!";
+    return (
+      <div>
+        <h1>React Unity WebGL Test</h1>
+        <this.myUnity />
+      </div>
+    );
   }
 }
