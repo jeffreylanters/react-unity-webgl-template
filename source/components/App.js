d3f6cd385b9f920d79d71ac1a5e9cecfabc91c34
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-06-24 22:04:24 +0200

    Added event sending and listening

--- source/components/App.js
+++ source/components/App.js
@@ -4,28 +4,46 @@ import Unity, { UnityContent, UnityVersion } from "../../../React-Unity-WebGL/";
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      rotation: 0
+    };
+    this.speed = 30;
     this.unityContent = new UnityContent(
-      "Public/Build/myGame.json",
-      "Public/Build/UnityLoader.js"
+      "unity_project_build/Build.json",
+      "unity_project_build/UnityLoader.js"
     );
-    this.unityContent.on("onLoad", () => {
-      console.log("Wow! did load");
+    this.unityContent.on("Say", message => {
+      console.log("Wow Unity said: " + message);
     });
-    window.setTimeout(() => {
-      this.unityContent.send("1", "2", "3");
-    }, 1000);
+    this.unityContent.on("SendRotation", rotation => {
+      this.setState({ rotation: Math.round(rotation) });
+    });
+  }
+  onClickStart() {
+    this.unityContent.send("mesh-crate", "StartRotation");
+  }
+  onClickStop() {
+    this.unityContent.send("mesh-crate", "StopRotation");
+  }
+  onClickFaster() {
+    this.speed += 10;
+    this.unityContent.send("mesh-crate", "SetRotationSpeed", this.speed);
+  }
+  onClickSlower() {
+    this.speed -= 10;
+    this.unityContent.send("mesh-crate", "SetRotationSpeed", this.speed);
   }
 
   render() {
     return (
       <div>
         <h1>React Unity WebGL Test</h1>
-        <Unity
-          unityContent={this.unityContent}
-          className="test"
-          width="300px"
-          height="200px"
-        />
+        <p>Rotation: {this.state.rotation}deg</p>
+        <button onClick={this.onClickStart.bind(this)}>{"Start"}</button>
+        <button onClick={this.onClickStop.bind(this)}>{"Stop"}</button>
+        <button onClick={this.onClickFaster.bind(this)}>{"Faster"}</button>
+        <button onClick={this.onClickSlower.bind(this)}>{"Slower"}</button>
+        <Unity unityContent={this.unityContent} />
       </div>
     );
   }
