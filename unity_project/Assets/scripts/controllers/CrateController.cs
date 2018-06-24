17e4097bcefb2a64d01b71c3f1f2a2155f4d086f
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-06-24 16:21:07 +0200

    Two example controller

---
+++ unity_project/Assets/scripts/controllers/CrateController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class CrateController : MonoBehaviour {
+
+	private float rotationSpeed = 30;
+	private bool isRotating = true;
+
+	[DllImport("__Internal")]
+	private static extern void SendRotation(float rotation);
+
+	private void Update () {
+		if (this.isRotating == true) {
+			this.transform.Rotate(new Vector3(0, this.rotationSpeed * Time.deltaTime));
+			SendRotation (this.transform.localEulerAngles.y);
+		}
+	}
+
+	public void SetRotationSpeed (float rotationSpeed) {
+		this.rotationSpeed = rotationSpeed;
+	}
+
+	public void StopRotation () {
+		this.isRotating = false;
+	}
+
+	public void StartRotation () {
+		this.isRotating = true;
+	}
+}
