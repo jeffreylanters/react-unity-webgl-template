987ee9603253d268fa8566f5ca29764d7a43a874
Jeffrey Lanters <jeffreylanters@me.com>
2020-08-24 16:00:44 +0200

    Reworked Test for ReactUnityWebGL 8.x

--- src/index.js
+++ src/index.js
@@ -1,5 +1,80 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Unity, { UnityContext } from "react-unity-webgl";
+import "./index.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.speed = 30;
+    this.state = {
+      degrees: 0,
+      message: "-",
+      showUnity: true,
+    };
+    this.unityContext = new UnityContext({
+      codeUrl: "/build/myunityapp.wasm",
+      frameworkUrl: "/build/myunityapp.framework.js",
+      dataUrl: "/build/myunityapp.data",
+      loaderUrl: "/build/myunityapp.loader.js",
+    });
+    this.unityContext.on("RotationDidUpdate", (degrees) => {
+      this.setState({ degrees: Math.round(degrees) });
+    });
+    this.unityContext.on("Say", (message) => {
+      this.setState({ message });
+    });
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>React UnityWebGL</h1>
+        <p>Rotation {this.state.degrees}deg</p>
+        <p>Last Said {this.state.message}</p>
+        <button
+          children={"Start Rotation"}
+          onClick={() => this.unityContext.send("mesh-crate", "StartRotation")}
+        />
+        <button
+          children={"Stop Rotation"}
+          onClick={() => this.unityContext.send("mesh-crate", "StopRotation")}
+        />
+        <button
+          children={"Faster Rotation"}
+          onClick={() => {
+            this.speed += 5;
+            this.unityContext.send(
+              "mesh-crate",
+              "SetRotationSpeed",
+              this.speed
+            );
+          }}
+        />
+        <button
+          children={"Slower Rotation"}
+          onClick={() => {
+            this.speed -= 5;
+            this.unityContext.send(
+              "mesh-crate",
+              "SetRotationSpeed",
+              this.speed
+            );
+          }}
+        />
+        <button
+          children={"(re)Unmount"}
+          onClick={() => this.setState({ showUnity: !this.state.showUnity })}
+        />
+        <div>
+          {this.state.showUnity === true ? (
+            <Unity width={"100%"} unityContext={this.unityContext} />
+          ) : (
+            <div />
+          )}
+        </div>
+      </Fragment>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
