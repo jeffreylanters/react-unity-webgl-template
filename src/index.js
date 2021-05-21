9159834071535f53d90ad5889ed624dcf27b21f7
Jeffrey Lanters <jeffreylanters@me.com>
2021-05-21 10:01:52 +0200

    Removed class components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,64 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-// import { TestFuncComponent } from "./test-func-component";
-import { TestClassComponent } from "./test-class-component";
+import Unity, { UnityContext } from "react-unity-webgl";
 
-class App extends Component {
-  render() {
-    // return <TestFuncComponent />;
-    return <TestClassComponent />;
-  }
-}
+const unityContext = new UnityContext({
+  loaderUrl: "build/myunityapp.loader.js",
+  dataUrl: "build/myunityapp.data",
+  frameworkUrl: "build/myunityapp.framework.js",
+  codeUrl: "build/myunityapp.wasm",
+});
+
+window.u = unityContext;
+
+unityContext.on("RotationDidUpdate", (degrees) => {
+  console.log({ degrees });
+});
+unityContext.on("Say", (message) => {
+  console.log({ message });
+});
+unityContext.on("progress", (progression) => {
+  console.log({ progression });
+});
+unityContext.on("canvas", (canvas) => {
+  console.log({ canvas });
+});
+unityContext.on("ClickedPosition", (x, y) => {
+  console.log({ x, y });
+});
+unityContext.on("error", (message) => {
+  console.log("An error!", message);
+});
+unityContext.on("log", (message) => {
+  console.log("A message!", message);
+});
+
+export const App = () => {
+  return (
+    <Fragment>
+      <div>
+        <button
+          children={"Start Rotation"}
+          onClick={() => unityContext.send("MeshCrate", "StartRotation")}
+        />
+        <button
+          children={"Stop Rotation"}
+          onClick={() => unityContext.send("MeshCrate", "StopRotation")}
+        />
+      </div>
+      <Unity
+        unityContext={unityContext}
+        // devicePixelRatio={1}
+        matchWebGLToCanvasSize={false}
+        style={{
+          width: "600px",
+          height: 400,
+          border: "2px solid black",
+          background: "grey",
+        }}
+      />
+    </Fragment>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
