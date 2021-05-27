7f71f247495e2c35eae02be6ee9ed64fbf1fa071
Jeffrey Lanters <jeffreylanters@me.com>
2021-05-27 23:08:42 +0200

    Updated to RUW 8.4.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import Unity, { UnityContext } from "react-unity-webgl";
+import Unity from "react-unity-webgl";
+import { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
   loaderUrl: "build/myunityapp.loader.js",
@@ -9,46 +10,59 @@ const unityContext = new UnityContext({
   codeUrl: "build/myunityapp.wasm",
 });
 
-window.u = unityContext;
+window.ctx = unityContext;
 
-unityContext.on("RotationDidUpdate", (degrees) => {
-  console.log({ degrees });
-});
-unityContext.on("Say", (message) => {
-  console.log({ message });
-});
-unityContext.on("progress", (progression) => {
-  console.log({ progression });
-});
-unityContext.on("canvas", (canvas) => {
-  console.log({ canvas });
-});
-unityContext.on("ClickedPosition", (x, y) => {
-  console.log({ x, y });
-});
-unityContext.on("error", (message) => {
-  console.log("An error!", message);
-});
-unityContext.on("log", (message) => {
-  console.log("A message!", message);
-});
+function App() {
+  const [isLoaded, setLoaded] = useState(false);
+  const [progression, setProgression] = useState(0);
+  const [rotation, setRotation] = useState(0);
+  const [message, setMessage] = useState("");
+  const [clickedPosition, setClickedPosition] = useState({ x: 0, y: 0 });
 
-export const App = () => {
+  useEffect(function () {
+    unityContext.on("progress", setProgression);
+    unityContext.on("loaded", function () {
+      setLoaded(true);
+    });
+    unityContext.on("error", function (message) {
+      console.log("An error!", message);
+    });
+    unityContext.on("log", function (message) {
+      console.log("A message!", message);
+    });
+    unityContext.on("canvas", function (element) {
+      console.log("Canvas", element);
+    });
+    unityContext.on("RotationDidUpdate", setRotation);
+    unityContext.on("Say", setMessage);
+    unityContext.on("ClickedPosition", function (x, y) {
+      setClickedPosition({ x, y });
+    });
+  }, []);
+
+  function startRotation() {
+    unityContext.send("MeshCrate", "StartRotation");
+  }
+
+  function stopRotation() {
+    unityContext.send("MeshCrate", "StopRotation");
+  }
+
   return (
     <Fragment>
-      <div>
-        <button
-          children={"Start Rotation"}
-          onClick={() => unityContext.send("MeshCrate", "StartRotation")}
-        />
-        <button
-          children={"Stop Rotation"}
-          onClick={() => unityContext.send("MeshCrate", "StopRotation")}
-        />
-      </div>
+      <h1>React Unity WebGL Test</h1>
+      <p>Loading {progression * 100} percent...</p>
+      {isLoaded === true && <p>Loaded!</p>}
+      <p>Rotation {rotation}deg</p>
+      <p>Message {message}</p>
+      <p>
+        Clicked Position {clickedPosition.x}, {clickedPosition.y}
+      </p>
+      <button children={"Start Rotation"} onClick={startRotation} />
+      <button children={"Stop Rotation"} onClick={stopRotation} />
+      <br />
       <Unity
         unityContext={unityContext}
-        // devicePixelRatio={1}
         matchWebGLToCanvasSize={false}
         style={{
           width: "600px",
@@ -59,6 +73,6 @@ export const App = () => {
       />
     </Fragment>
   );
-};
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
