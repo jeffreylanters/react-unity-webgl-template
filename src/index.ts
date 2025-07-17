e33f66a51c074482fa325d30de286fb961df74ff
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-17 12:49:59 +0200

    chore: remove unused index.ts file from the project

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { createElement } from "react";
-import { App } from "./app.component";
-import "./styles.css";
-
-const element = document.getElementById("root")!;
-const root = createRoot(element);
-
-root.render(createElement(App));
